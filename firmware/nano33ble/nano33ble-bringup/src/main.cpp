f229189193ae1f6aad5e1e3c30a5653854a5f980
Alexx0597 <alexxouil@gmail.com>
2026-03-27 15:00:30 +0100

    Gitignore update to not track data files & minor update of firmware

--- firmware/nano33ble/nano33ble-bringup/src/main.cpp
+++ firmware/nano33ble/nano33ble-bringup/src/main.cpp
@@ -8,7 +8,7 @@ void setup() {
     Serial.begin(230400);
     delay(1500);
 
-    Serial.println("time_ms,ax,ay,az,gx,gy,gz");
+    Serial.println("gx,gy,gz,ax,ay,az");
 }
 
 void loop() {
@@ -21,22 +21,23 @@ void loop() {
         bool accelReady = IMU.accelerationAvailable();
         bool gyroReady  = IMU.gyroscopeAvailable();
 
-        if (accelReady) {
-            IMU.readAcceleration(ax, ay, az);
-        }
-
         if (gyroReady) {
             IMU.readGyroscope(gx, gy, gz);
         }
 
-        if (accelReady || gyroReady) {
-            Serial.print(lastReadMs); Serial.print(",");
-            Serial.print(ax, 4); Serial.print(",");
-            Serial.print(ay, 4); Serial.print(",");
-            Serial.print(az, 4); Serial.print(",");
+        if (accelReady) {
+            IMU.readAcceleration(ax, ay, az);
+        }
+
+        
+
+        if (accelReady || gyroReady) {            
             Serial.print(gx, 4); Serial.print(",");
             Serial.print(gy, 4); Serial.print(",");
-            Serial.println(gz, 4);
+            Serial.print(gz, 4); Serial.print(",");
+            Serial.print(ax, 4); Serial.print(",");
+            Serial.print(ay, 4); Serial.print(",");
+            Serial.println(az, 4);
         }
     }
 }
